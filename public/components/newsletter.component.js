717bbd74cb69d85cd4846ac6e983b4211539bc66
Haley Kalb <haley.kalb@gmail.com>
2017-07-02 21:41:42 -0600

    added newsletter form submission

--- public/components/newsletter.component.js
+++ public/components/newsletter.component.js
@@ -19,5 +19,52 @@
       $('.brand-logo').show()
     }
 
+// MAIL CHIMP API
+    // let mailChimp = function mailChimp(data) {
+    // }
+    //
+    // $("#signUpForm").click(function(event) {
+    //   event.preventDefault();
+    //   if ($(event.target).hasClass("newsletter")) {
+    //     event.preventDefault();
+    //     let currentLinkElement = event.target;
+    //     // let searchText = $('.myBar').val();
+    //     // $(".myBar").val("");
+    //     //take out spaces in search criteria (replace space with empty string)
+    //     let data = 'JSON Request Data';
+    //     let request = $.ajax({
+    //       url: `https://us16.api.mailchimp.com/3.0/lists`,
+    //       method: "GET",
+    //       dataType: 'json',
+    //       data: JSON.stringify(data),
+    //       success(innerData) {
+    //         mailChimp(innerData);
+    //         console.log(innerData);
+    //         let getMail = [];
+    //         let mailInfo = {};
+    //         console.log("getMail", getMail);
+    //         console.log("mailInfo", mailInfo);
+    //         for (let i = 0; i < innerData.length; i++) {
+    //           if (innerData[i] === undefined) {
+    //           }
+    //           else {
+    //             getMail.push(innerData[i]);
+    //           }
+    //         }
+    //       },
+    //       error() {
+    //         console.log("error")
+    //       }
+    //
+    //     })
+    //
+    //     return (request.then(function(resp) {
+    //       return resp;
+    //     }, function(err) {
+    //       return err;
+    //     }));
+    //   }
+    // })
+
   } // END Newsletter Controller
 }());
